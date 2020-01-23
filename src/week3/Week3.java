45cf0ddd5b5f9c2cdc6aca36181f9df6638df553
truonngh <truonngh@sheridancollege.ca>
2020-01-23 10:05:17 -0500

    student code

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -17,6 +17,7 @@ public class Week3 {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("lallalla");
+        System.out.println("lallalala");
     }
     
 }
