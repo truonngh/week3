4cdf45acdaa0edcb00b2ac475bb461e97134bebb
truonngh <truonngh@sheridancollege.ca>
2020-01-23 10:27:24 -0500

    student code

--- src/week3/Week3.java
+++ src/week3/Week3.java
@@ -1,7 +1,7 @@
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * and open the template in the editor. branch
  */
 package week3;
 
@@ -16,8 +16,7 @@ public class Week3 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("lallalla");
-        System.out.println("lallalala");
+        
     }
     
 }
